0f26cbdc87f225bb6c386507166de1f21a8c4e44
kaziabulhasib <abulhasibkazi@gmail.com>
2024-06-30 22:45:48 +0530

    react tool tip post title postcard

--- src/Components/PostCard/PostCard.jsx
+++ src/Components/PostCard/PostCard.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
+import { Tooltip } from "react-tooltip";
 const PostCard = ({ post }) => {
   const {
     _id,
@@ -50,39 +50,55 @@ const PostCard = ({ post }) => {
     <div>
       <div
         onClick={handleClick}
-        className='p-6 mx-auto space-y-12 border rounded-lg hover:cursor-pointer  h-[375px]'>
+        className='p-6 mx-auto space-y-12 border rounded-lg hover:cursor-pointer  '>
         <article className='space-y-8 '>
           <div className='space-y-6'>
             {/* post title */}
 
-            <h1
-              className='text-xl font-bold md:text-2xl text-left h-24 '
-              title={postTitle}>
-              {limitedTitle}
-            </h1>
-            <div className='flex text-left lg:flex-row flex-col   lg:items-center justify-between w-full   '>
+            <div id='my-anchor-element' data-tooltip-content={postTitle}>
+              {" "}
+              <h1
+                className='text-xl font-bold md:text-2xl text-left h-16 '
+                // title={postTitle}
+              >
+                {limitedTitle}
+              </h1>
+            </div>
+
+            <Tooltip anchorSelect='#my-anchor-element' />
+            <div>
               {/* Author image */}
-              <div className='flex items-center md:space-x-2'>
-                •
-                <img
-                  src={authorImage}
-                  alt=''
-                  className='lg:w-12 lg:h-12 w-6 h-6 border rounded-full '
-                />
+              <div className='flex justify-between   items-center md:space-x-2'>
+                <div className='flex gap-4 items-center  '>
+                  {" "}
+                  <img
+                    src={authorImage}
+                    alt=''
+                    className='lg:w-12 lg:h-12 w-6 h-6 border rounded-full '
+                  />
+                  <h1 className='text-blue-700 underline '>{authorName}</h1>
+                </div>
                 {/* Time */}
-                <p className='text-sm'>• {formatPostTime(postTime)}</p>
+                <p className='text-sm text-blue-950'>
+                  {" "}
+                  {formatPostTime(postTime)}
+                </p>
               </div>
               {/* votes & comments count */}
-              <p className='flex-shrink-0 mt-3 text-sm md:mt-0'>
+              {/* <p className='flex-shrink-0 mt-3 text-sm md:mt-0'>
                 • {commentCount || 0} comments • {upVote + downVote} votes •
-              </p>
+              </p> */}
             </div>
           </div>
         </article>
-        <div>
-          <h1 className='bg-gray-700 text-white text-center px-2 py-2 w-2/3 mx-auto font-semibold rounded-md text-xl tracking-widest'>
+        <div className='flex items-center  justify-evenly gap-2 border p-2 '>
+          <h1 className='  text-center    font-semibold rounded-md text-xl tracking-widest'>
             #{postTag}
           </h1>
+          {/* votes & comments count */}
+          <p className='flex-shrink-0 mt-3  md:mt-0'>
+            {commentCount || 0} comments • {upVote + downVote} votes
+          </p>
         </div>
       </div>
     </div>
